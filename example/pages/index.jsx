0bd40a279def88d29f520e97b75ff33b70351abb
tokuda109 <tokuda109@gmail.com>
2019-07-11 15:51:12 +0900

    feat(example): apply styles to example & update next version

--- example/pages/index.jsx
+++ example/pages/index.jsx
@@ -1,4 +1,4 @@
-
+import Head from 'next/head'
 import { withCookie } from 'next-cookie'
 
 class IndexPage extends React.Component {
@@ -10,34 +10,72 @@ class IndexPage extends React.Component {
   }
 
   static async getInitialProps(ctx) {
-    ctx.cookie.set('get-initial-props', 'test')
+    ctx.cookie.set('getInitialProps', 'This value is set by getInitialProps.')
 
-    const name = ctx.cookie.get('name')
+    return {}
+  }
 
-    let displayName = 'no name'
-    if (name) {
-      displayName = name
-    }
-
-    return { displayName, test: ctx.cookie.get('test') }
+  componentDidMount() {
   }
 
   render() {
-    const { cookie, displayName, test } = this.props
-    const { name } = this.state
+    const { cookie } = this.props
+    const { storeKey, storeVal, removeKey } = this.state
+
+    const all = cookie.getAll()
+
+    let text = ""
+    if (Object.keys(all).length > 0) {
+      text = JSON.stringify(all)
+    }
 
     return (
-      <div>
-        <p>Display name: { displayName }, test: { test }</p>
+      <div className="container">
+        <Head>
+          <link rel="stylesheet" href="/static/app.css" />
+        </Head>
 
-        <input type="text" name="name" onChange={ e => this.setState({ name: e.target.value }) } />
-        <a onClick={ () => {
-          cookie.set('name', name)
-        } }>Store name to cookie</a>
-        <br />
-        <a onClick={ () => {
-          cookie.remove('name')
-        } }>Remove name</a>
+        <section className="section">
+          <p className="section__text">Store value by key.</p>
+
+          <input
+            type="text"
+            name="key"
+            placeholder="Cookie key"
+            onChange={ e => this.setState({ storeKey: e.target.value }) } />
+          <input
+            type="text"
+            name="val"
+            placeholder="Cookie value"
+            onChange={ e => this.setState({ storeVal: e.target.value }) } />
+
+          <button onClick={ () => {
+            if (storeKey && storeVal) {
+              cookie.set(storeKey, storeVal)
+            }
+          } }>Store</button>
+        </section>
+
+        <section className="section">
+          <p className="section__text">Delete cookie by key.</p>
+
+          <input
+            type="text"
+            name="key"
+            placeholder="Cookie key"
+            onChange={ e => this.setState({ removeKey: e.target.value }) } />
+
+          <button onClick={ () => {
+            if (removeKey) {
+              cookie.remove(removeKey)
+            }
+          } }>Delete</button>
+        </section>
+
+        <section className="section">
+        <p className="section__text">Cookie result:</p>
+          <textarea value={(() => text)()} rows={10} readOnly onChange={() => {}}></textarea>
+        </section>
       </div>
     )
   }
